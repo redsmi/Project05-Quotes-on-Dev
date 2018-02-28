11781aa458770a20ae63560c33fd54624bce72da
David Yin <davidyin92@hotmail.com>
2018-02-28 14:27:06 -0800

    eslint include api_vars

---
+++ js/api.js
@@ -0,0 +1,13 @@
+(function($){
+
+// fetch a random quote post
+
+$('body').append('');
+
+// history api, 
+// history.pushState() push variable with slug to update url
+// so back/forward works
+
+// submit a new quote with the forum using ajax
+
+})(jQuery);
