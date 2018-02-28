5c720a8b4cdf9c694c214268fd8f9b0cb447a161
David Yin <davidyin92@hotmail.com>
2018-02-28 10:27:20 -0800

    Footer nav menu

--- footer.php
+++ footer.php
@@ -11,7 +11,20 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+
+					<?php 
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary',
+							'menu_id' => 'primary-menu',
+							'menu_class' => 'footer-navigation'
+						)
+					);
+					?>
+
+					<p>Brought to you by &copy; <a href="https://github.com/redsmi/Project05-Quotes-on-Dev">redsmi</a>
+					<?php echo date('Y'); ?></p>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
