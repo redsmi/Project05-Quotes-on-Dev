e88fd0df8adf3a08b0844eb913109f3561263a29
David Yin <davidyin92@hotmail.com>
2018-02-28 14:16:10 -0800

    Edit content.php, extras.php filter andaction

--- searchform.php
+++ searchform.php
@@ -1,10 +1,14 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<label>
-			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" class="search-field" 
+			placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+			<?php echo esc_html( 'Search edit' ); ?>
+			<span class="icon-search">
+				<i class="fa fa-search"></i>
+			</span>
 		</button>
 	</fieldset>
 </form>
