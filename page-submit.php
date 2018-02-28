0ed2dc53413e2e8da2b5f206f27eda7015ba3251
David Yin <davidyin92@hotmail.com>
2018-02-28 11:19:16 -0800

    Submit page: Forms, labels, conditional if user is logged in

--- page-submit.php
+++ page-submit.php
@@ -10,13 +10,50 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+            <section class="quote-submit-wrapper">
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+                <header>
+                    <?php the_title('<h1 class="entry-title">'); ?>
+                </header>
 
-			<?php endwhile; // End of the loop. ?>
+
+                <?php 
+                    if ( is_user_logged_in() && current_user_can( 'edit_posts' ) ):
+                ?>
+                <form name="quoteForm" id="quote-submit-form">
+                    <div>
+                        <label for="quote-author">Author of Quote</label>
+                        <input type="text" name="quote_author" id="quote-author">
+                    </div>
+                    <div>
+                        <label for="quote-content">Quote</label>
+                        <textarea name="quote_content" id="quote-content" cols="20" rows="3"></textarea>
+                    </div>
+                    <div>
+                        <label for="quote-source">Where did you find this quote? (e.g. book name)</label>
+                        <input type="text" name="quote_source" id="quote-source">
+                    </div>
+                    <div>
+                        <label for="quote-source-url">Provide the URL of the quote source, if available.</label>
+                        <input type="url" name="quote_source_url" id="quote-source-url">
+                    </div>
+
+                    <input type="submit" value="Submit Quote">
+                </form>
+                <p class="submit-success-message" style="display:none;"></p>
+
+                <?php else: ?>
+                        <p>Sorry, you must be logged in to submit a quote!</p>
+                        <p>
+                            <?php echo sprintf( '<a href="%1s">%2s</a>', 
+                            esc_url( wp_login_url() ), 'Click here to login.' ); ?>
+                        </p>
+                <?php endif; ?>
+            </section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
+
+<!-- .on('click') $('#quote-submit-form').submit(); -->
