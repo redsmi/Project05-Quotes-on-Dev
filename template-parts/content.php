670047918e71860813012a7a076d8453356ca178
David Yin <davidyin92@hotmail.com>
2018-03-01 09:48:37 -0800

    Home, Mobile Tablet CSS adjustments

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,7 +20,7 @@ $source_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', true);
 		<?php the_title( '<h2 class="entry-title">&mdash; ', '</h2>' ); ?>
 
 		<?php if( $source && $source_url ): ?>
-			<span class="source">
+			<span class="source">,
 				<a href="<?php echo $source_url; ?>">
 				<?php echo $source ?>
 				</a>
